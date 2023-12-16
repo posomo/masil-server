2dcd282e41076079588e1e406d0e36892c075b7c
johan1103 <johan1103@naver.com>
2023-12-17 04:59:51 +0900

    feaure: add mybatis sample

---
+++ src/main/java/com/posomo/masil/mapper/User.java
@@ -0,0 +1,14 @@
+package com.posomo.masil.mapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User {
+  private Long id;
+  private String name;
+}
