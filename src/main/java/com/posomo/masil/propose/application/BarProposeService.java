7af48b88b8158c983be12f529163602a056e62eb
inhyeok <alzpqm@naver.com>
2023-12-27 18:19:21 +0900

    fix: add transactional annotation to Service Classes

--- src/main/java/com/posomo/masil/propose/application/BarProposeService.java
+++ src/main/java/com/posomo/masil/propose/application/BarProposeService.java
@@ -1,6 +1,7 @@
 package com.posomo.masil.propose.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.posomo.masil.propose.controller.dto.BarProposeRequestDto;
 import com.posomo.masil.propose.domain.BarPropose;
@@ -14,6 +15,7 @@ public class BarProposeService {
 
 	private final BarProposeRepository barProposeRepository;
 
+	@Transactional
 	public void proposeBar(BarProposeRequestDto barProposeRequestDto) {
 		BarPropose barPropose = barProposeRequestDto.mapToDomain();
 		barProposeRepository.save(barPropose);
