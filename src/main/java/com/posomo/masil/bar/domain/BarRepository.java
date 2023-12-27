f52e46fdd1c5fa464966e2a78fdac4914b932bd3
inhyeok <alzpqm@naver.com>
2023-12-27 18:19:04 +0900

    fix: fix Bar Domain PK Type

--- src/main/java/com/posomo/masil/bar/domain/BarRepository.java
+++ src/main/java/com/posomo/masil/bar/domain/BarRepository.java
@@ -3,5 +3,5 @@ package com.posomo.masil.bar.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface BarRepository extends JpaRepository<Bar, Long>, BarQuerydsl, QuerydslPredicateExecutor<Bar> {
+public interface BarRepository extends JpaRepository<Bar, String>, BarQuerydsl, QuerydslPredicateExecutor<Bar> {
 }
