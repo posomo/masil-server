06cf008e43d5bfba0764bfc4344968caa36bfbe8
johan1103 <johan1103@naver.com>
2024-01-28 00:45:39 +0900

    feat: detail 정보 return하는 readonly method 구현

--- src/main/java/com/posomo/masil/bar/application/BarService.java
+++ src/main/java/com/posomo/masil/bar/application/BarService.java
@@ -2,6 +2,7 @@ package com.posomo.masil.bar.application;
 
 import java.util.List;
 
+import com.posomo.masil.bar.domain.vo.BarOpeningHour;
 import com.posomo.masil.bar.dto.MapBarResponseDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,5 +30,15 @@ public class BarService {
 	@Transactional(readOnly = true)
 	public List<Bar> retrieveBarForMap(double longitude, double latitude, double distance, int limit){
 		return barRepository.findBarByLocation(longitude,latitude,distance,limit);
+	}
+
+	@Transactional(readOnly = true)
+	public Bar retrieveBarForDetail(String vendorId){
+		return barRepository.findBarByIdJoinFetchAll(vendorId).orElseThrow(RuntimeException::new);
+	}
+
+	@Transactional(readOnly = true)
+	public List<BarOpeningHour> openingHours(String vendorId){
+		return
 	}
 }
