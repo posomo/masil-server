5fd744435adb683e35b8c069e16afbcb809662d6
johan1103 <johan1103@naver.com>
2024-01-28 01:29:31 +0900

    feat: detail controllerr 완성

--- src/main/java/com/posomo/masil/bar/controller/DetailController.java
+++ src/main/java/com/posomo/masil/bar/controller/DetailController.java
@@ -1,11 +1,15 @@
 package com.posomo.masil.bar.controller;
 
 import com.posomo.masil.bar.application.BarService;
+import com.posomo.masil.bar.domain.Bar;
+import com.posomo.masil.bar.domain.vo.BarOpeningHour;
 import com.posomo.masil.bar.dto.BarDetailResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/detail")
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DetailController {
   private final BarService barService;
   public BarDetailResponseDto detail(@RequestParam(value = "vendor-id")String vendorId){
-
+    Bar bar = barService.retrieveBarForDetail(vendorId);
+    List<BarOpeningHour> openingHour = barService.openingHours(vendorId);
+    return BarDetailResponseDto.of(bar,openingHour);
   }
 }
