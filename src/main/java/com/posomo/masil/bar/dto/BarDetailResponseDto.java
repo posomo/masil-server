8da25748b042dee1672fc131b530771a225d4a64
johan1103 <johan1103@naver.com>
2024-01-28 00:46:24 +0900

    feat: detail api 응답용 dto 작성

--- src/main/java/com/posomo/masil/bar/dto/BarDetailResponseDto.java
+++ src/main/java/com/posomo/masil/bar/dto/BarDetailResponseDto.java
@@ -1,2 +1,33 @@
-package com.posomo.masil.bar.dto;public class BarDetailResponseDto {
+package com.posomo.masil.bar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class BarDetailResponseDto {
+  private String name;
+  private List<String> photos;
+  private String roadAddress;
+  private String localAddress;
+  private String commonAddress;
+  private OpeningHour openingHour;
+  private List<String> keywords;
+  private String category;
+  private List<MenuInfo> menus;
+
+  private static class OpeningHour{
+    private String[] openTime = new String[7];
+    private String[] closeTime = new String[7];
+  }
+  private static class MenuInfo{
+    private String name;
+    private String description;
+    private Integer price;
+  }
+
 }
