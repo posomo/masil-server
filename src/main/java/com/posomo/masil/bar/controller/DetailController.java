b1bf30ee0f96cb58819d50d49663c4c04c133ee3
johan1103 <johan1103@naver.com>
2024-01-28 00:47:05 +0900

    ㄹãfeat: detail 응답ì용 controller ì구현

--- src/main/java/com/posomo/masil/bar/controller/DetailController.java
+++ src/main/java/com/posomo/masil/bar/controller/DetailController.java
@@ -1,2 +1,18 @@
-package com.posomo.masil.bar.controller;public class DetailController {
+package com.posomo.masil.bar.controller;
+
+import com.posomo.masil.bar.application.BarService;
+import com.posomo.masil.bar.dto.BarDetailResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/detail")
+@RequiredArgsConstructor
+public class DetailController {
+  private final BarService barService;
+  public BarDetailResponseDto detail(@RequestParam(value = "vendor-id")String vendorId){
+
+  }
 }
